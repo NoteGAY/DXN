a5383377834665b7420872f5a18f7f5681e1808a
NoteGAY <52817998+NoteGAY@users.noreply.github.com>
2019-07-12 22:12:29 +0800

    Update Demo1.java

--- src/com/alibaba/git/Demo1.java
+++ src/com/alibaba/git/Demo1.java
@@ -3,5 +3,6 @@ package com.alibaba.git;
 public class Demo1 {
     public static void main(String[] args) {
         System.out.println("1111111111111111111");
+        System.out.println("2222222222222222222");
     }
 }
