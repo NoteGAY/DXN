dff11b29fa0335e224fc53bd700ea0a7cab9be51
NoteGAY <52817998+NoteGAY@users.noreply.github.com>
2020-04-09 15:24:45 +0800

    Delete Demo1.java

--- src/com/alibaba/git/Demo1.java
+++
@@ -1,8 +0,0 @@
-package com.alibaba.git;
-
-public class Demo1 {
-    public static void main(String[] args) {
-        System.out.println("1111111111111111111");
-        System.out.println("2222222222222222222");
-    }
-}
